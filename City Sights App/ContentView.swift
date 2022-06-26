5893729e744a9387d32bdb0d1e1816715826bea5
luane <luaneemiliano@gmail.com>
2022-06-26 08:33:40 -0700

    Initial Commit

---
+++ City Sights App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  City Sights App
+//
+//  Created by luane Niejelski on 6/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
